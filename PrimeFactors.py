bb75d7fcdf76c8ec6a87854678daf51f68279ecd
Hyunwoo Park <dshw.park@samsung.com>
2024-06-13 10:09:04 +0900

    [feature] 25

--- PrimeFactors.py
+++ PrimeFactors.py
@@ -1,4 +1,6 @@
 class PrimeFactors:
     def of(self, n: int) -> []:
         result = []
+        if n == 2:
+            result.append(2)
         return result
--- test_PrimeFactors.py
+++ test_PrimeFactors.py
@@ -6,3 +6,7 @@ class Test(TestCase):
     def test_get_prime_factors_1(self):
         p = PrimeFactors()
         self.assertEqual([], p.of(1))
+
+    def test_get_prime_factors_2(self):
+        p = PrimeFactors()
+        self.assertEqual([2], p.of(2))
