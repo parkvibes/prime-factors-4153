677e50fb4a68ea85a02c0c45fc62e43b4ac44adc
Hyunwoo Park <dshw.park@samsung.com>
2024-06-13 10:33:49 +0900

    [refactoring] 50

--- PrimeFactors.py
+++ PrimeFactors.py
@@ -1,14 +1,10 @@
 class PrimeFactors:
     def of(self, n: int) -> []:
         result = []
-        if n > 1:
-            divisor = 2
-            if n == 4 or n == 6 or n == 9 or n == 12:
-                while n > 1:
-                    while n % divisor == 0:
-                        result.append(divisor)
-                        n //= divisor
-                    divisor += 1
-            else:
-                result.append(n)
+        divisor = 2
+        while n > 1:
+            while n % divisor == 0:
+                result.append(divisor)
+                n //= divisor
+            divisor += 1
         return result
